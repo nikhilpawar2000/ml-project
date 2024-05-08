f4f5e45fb981f0fe2b1c8b1bb4bfd216b6f78322
nikhilpawar2000 <nikhilpawar08012000@gmail.com>
2024-05-08 12:16:31 +0530

    Data transformation done

--- src/exception.py
+++ src/exception.py
@@ -2,8 +2,8 @@ import sys
 from src.logger import logging
 
 def erroe_message_detail(error,error_detail:sys):
-    _,_exc_tb=error_detail.exc_info()
-    file_name=_exc_tb.tb_frame.f_code.co_filename
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="error occuredin python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
     )
